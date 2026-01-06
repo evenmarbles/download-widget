1826e3baede38a794ddc6cf7dcdd838a2884cd99
evenmarbles <ajackson.us@gmail.com>
2026-01-06 13:32:50 -0500

    Update embed_download_brochure.js

--- embed_download_brochure.js
+++ embed_download_brochure.js
@@ -48,23 +48,21 @@
         </div>
 
         <div class="bpw-success" aria-live="polite">
-          <div class=bpw-framewrap>
-            <div class="bpw-card">
-              <h2 class="bpw-title">Brochure successfully sent!</h2>
-              <div style="text-align: center; margin-top: 15px;">
-                <svg width="104" height="100" viewBox="0 0 104 100" class="tw-inline tw-align-baseline wt-icon" style="width: 80px; height: 80px;">
-                  <g stroke-width="3" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M91.16 34.168A46.13 46.13 0 0194.728 52c0 25.678-20.685 46.364-46.363 46.364C22.685 98.364 2 77.678 2 52 2 26.322 22.685 5.636 48.364 5.636c9.094 0 17.653 2.675 24.786 7.133" stroke="#DDDEDF">
-                    </path><path stroke="#1775ba" d="M29.273 37.152l18.182 17.575L102 2"></path>
-                  </g>
-                </svg>
-              </div>
-              <p class="bpw-text">
-                We&#039;ve sent the Suwannee River Sea Kayak Skills Expedition Brochure to
-                <span class="bpw-email" data-bpw-email></span>.
-                <br/>Check your inbox!
-              </p>
-              <button class="bpw-btn" type="button" data-bpw-close>Close</button>
+          <div class="bpw-card">
+            <h2 class="bpw-title">Brochure successfully sent!</h2>
+            <div style="text-align: center; margin-top: 15px;">
+              <svg width="104" height="100" viewBox="0 0 104 100" class="tw-inline tw-align-baseline wt-icon" style="width: 80px; height: 80px;">
+                <g stroke-width="3" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M91.16 34.168A46.13 46.13 0 0194.728 52c0 25.678-20.685 46.364-46.363 46.364C22.685 98.364 2 77.678 2 52 2 26.322 22.685 5.636 48.364 5.636c9.094 0 17.653 2.675 24.786 7.133" stroke="#DDDEDF">
+                  </path><path stroke="#1775ba" d="M29.273 37.152l18.182 17.575L102 2"></path>
+                </g>
+              </svg>
             </div>
+            <p class="bpw-text">
+              We&#039;ve sent the Suwannee River Sea Kayak Skills Expedition Brochure to
+              <span class="bpw-email" data-bpw-email></span>.
+              <br/>Check your inbox!
+            </p>
+            <button class="bpw-btn" type="button" data-bpw-close>Close</button>
           </div>
         </div>
       </div>
@@ -192,6 +190,7 @@
 
   window.BrevoPopup = { open: openPopup, close: closePopup };
 })();
+
 
 
 
